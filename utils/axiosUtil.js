7c58523ff3fadca016a5d7a2a1e624c512d34d1d
Shreyas Mudakavi <shreyasmudak@gmail.com>
2024-03-10 13:36:58 -0400

    added backend url

--- utils/axiosUtil.js
+++ utils/axiosUtil.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:5000",
+  baseURL: "https://estate-backend.adaptable.app/",
+  // baseURL: "http://localhost:5000",
 });
 
 export default axiosInstance;
